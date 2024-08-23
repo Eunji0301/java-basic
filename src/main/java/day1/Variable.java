26db928da0ba4a2732de1c5ac95a910a15aa204d
Eunji0301 <forstudy.eunji@gmail.com>
2024-08-23 10:45:18 +0900

    variable example

---
+++ src/main/java/day1/Variable.java
@@ -0,0 +1,65 @@
+package day1;
+
+public class Variable {
+    public static void main(String args[]) {
+        // 변수를 사용해서 값을 저장할 수 있다.
+
+        // 변수는 상자
+        // 상자는 이름이 있어야 함.
+        // 상자는 크기와 모양이 있어야 함.
+
+        // 숫자
+        // 상자(변수)를 만들어야 함.
+        int age;
+
+        // 값을 변수에 담아야 함.
+        // =을 이용해서 저장 가능
+        age = 20;
+
+        // 문자
+        String name;
+        name = "홍길동";
+
+        // 변수의 값을 꺼내서 사용
+        System.out.println(age);
+        System.out.println(name);
+
+        // 안녕하세요 저는 20살 홍길동입니다. 10번 출력.
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+
+        // 홍길동의 나이가 21살이 되었다. 21살로 바꿔서 10번 출력.
+        age = 21;
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+
+        // 홍길동이 홍길만으로 개명. 바뀐 이름 적용해서 10번 출력
+        name = "홍길만";
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + "입니다.");
+    }
+}
