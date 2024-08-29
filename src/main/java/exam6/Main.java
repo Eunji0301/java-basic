dd06057eaa2f94d96f8dd095c3d7abf116057efb
Eunji0301 <forstudy.eunji@gmail.com>
2024-08-29 09:47:25 +0900

    selectBeverage

--- src/main/java/exam6/Main.java
+++ src/main/java/exam6/Main.java
@@ -21,22 +21,27 @@ public class Main {
                 System.out.print("음료수를 선택해주세요 : ");
                 int target = Integer.parseInt(scanner.nextLine());
                 String beverage = vm.selectBeverage(target);
+                if(beverage.equals("잔액부족")){
+                    System.out.println("잔액이 부족합니다.");
+                    continue;
+                }
+
                 System.out.println(beverage + "을/를 뽑으셨습니다.");
-                int remainder = vm.getRemainder();
-                System.out.println("잔액은 " + remainder + "원입니다.");
+//                int remainder = vm.getRemainder();
+//                System.out.println("잔액은 " + remainder + "원입니다.");
             }
-            else if (option == 3) {
-                vm.printBeverage();
-            }
-            else if (option == 4) {
-                int remainder = vm.getRemainder();
-                System.out.println("현재 잔액은 " + remainder + "원입니다.");
-            }
-            else if (option == 5) {
-                int remainder = vm.getRemainder();
-                System.out.println("자판기 프로그램을 종료합니다. 남은 돈 " + remainder + "원이 반환됩니다.");
-                break;
-            }
+//            else if (option == 3) {
+//                vm.printBeverage();
+//            }
+//            else if (option == 4) {
+//                int remainder = vm.getRemainder();
+//                System.out.println("현재 잔액은 " + remainder + "원입니다.");
+//            }
+//            else if (option == 5) {
+//                int remainder = vm.getRemainder();
+//                System.out.println("자판기 프로그램을 종료합니다. 남은 돈 " + remainder + "원이 반환됩니다.");
+//                break;
+//            }
         }
     }
 }
--- src/main/java/exam6/VendingMachine.java
+++ src/main/java/exam6/VendingMachine.java
@@ -15,46 +15,32 @@ public class VendingMachine {
 
     // 값 세팅 X
     // 연습용으로 하나의 객체만 사용할 때에는 미리 넣어두는 것이 편하니까 그냥 넣어둠.
-    int sumMoney = 0;
-    int beveragePrice = 0;
+
     int remainderMoney = 0;
 
+    String[] beverageNames = {"콜라", "사이다", "커피"};
+    int[] beveragePrices = {1000, 1200, 800};
 
     public void inputMoney(int money) {
         remainderMoney += money;
     }
 
-    public String selectBeverage(int num) {
-        String beverageName = "";
-
-        if (num == 0) {
-            beverageName = "콜라";
-            beveragePrice = 1000;
-        } else if (num == 1) {
-            beverageName = "사이다";
-            beveragePrice = 1200;
-        } else if (num == 2) {
-            beverageName = "커피";
-            beveragePrice = 800;
-        }
-        return beverageName;
+    public String selectBeverage(int target) {
+       if(beveragePrices[target] > remainderMoney) {
+           return "잔액부족"; // return하게되면 메서드는 거기서 종료. 더 이상 밑으로 안내려감.
+       }
+       remainderMoney -= beveragePrices[target];
+       return beverageNames[target];
     }
 
-    public int getRemainder() {
-        if (sumMoney < beveragePrice) {
-            System.out.println("잔액이 부족합니다.");
-        } else {
-            remainderMoney = sumMoney - beveragePrice;
-            sumMoney = remainderMoney;
-        }
-        return sumMoney;
-    }
+//    public int getRemainder() {
+//        if (sumMoney < beveragePrice) {
+//            System.out.println("잔액이 부족합니다.");
+//        } else {
+//            remainderMoney = sumMoney - beveragePrice;
+//            sumMoney = remainderMoney;
+//        }
+//        return sumMoney;
+//    }
 
-    public int getAmount() {
-        if (num_coke < 0 || num_cidar < 0 || num_coffee < 0) {
-            System.out.println("수량이 부족합니다.");
-        }
-        e
-
-    }
 }
