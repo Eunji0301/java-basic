2ae6966266db737b04082a588e2050ce3c25ebc8
Eunji0301 <forstudy.eunji@gmail.com>
2024-08-30 14:16:25 +0900

    VendingMachineToObjectConstructor

---
+++ src/main/java/day4/exam6/Beverage.java
@@ -0,0 +1,13 @@
+package day4.exam6;
+
+public class Beverage {
+    String name;
+    int price;
+    int quantity;
+
+    public Beverage(String name, int price,  int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
--- src/main/java/day4/exam6/VendingMachine.java
+++ src/main/java/day4/exam6/VendingMachine.java
@@ -10,32 +10,22 @@ public class VendingMachine {
     // 연습용으로 하나의 객체만 사용할 때에는 미리 넣어두는 것이 편하니까 그냥 넣어둠.
 
     int remainderMoney = 0;
+    ArrayList<Beverage> beverages = new ArrayList<>();
 
-    //    String[] beverageNames = {"콜라", "사이다", "커피"};
-    ArrayList<String> beverageNames = new ArrayList<>() {{
-        add("콜라");
-        add("사이다");
-        add("커피");
-    }};
+    public VendingMachine() {
+        Beverage b1 = new Beverage("콜라",1000,1);
+        Beverage b2 = new Beverage("사이다",1200,2);
+        Beverage b3 = new Beverage("커피",800,1);
 
-    //    int[] beveragePrices = {1000, 1200, 800};
-    ArrayList<Integer> beveragePrices = new ArrayList<>() {{
-        add(1000);
-        add(1200);
-        add(800);
-    }};
-
-    //    int[] beverageNum = {1, 2, 1};
-    ArrayList<Integer> beverageNum = new ArrayList<>() {{
-        add(1);
-        add(2);
-        add(1);
-    }};
+        beverages.add(b1);
+        beverages.add(b2);
+        beverages.add(b3);
+    }
 
     public void printBeverage() {
         System.out.println("== 음료수 목록 ==");
-        for (int i = 0; i < beverageNames.size(); i++) {
-            System.out.println(i + ". " + beverageNames.get(i) + " : " + beveragePrices.get(i) + "원" + ", 남은 수량 : " + beverageNum.get(i));
+        for (int i = 0; i < beverages.size(); i++) {
+            System.out.println(i + ". " + beverages.get(i).name + " : " + beverages.get(i).price + "원" + ", 남은 수량 : " + beverages.get(i).quantity);
         }
         System.out.println("=================");
     }
@@ -45,22 +35,23 @@ public class VendingMachine {
     }
 
     public String selectBeverage(int target) {
-        if (beveragePrices.get(target) > remainderMoney) {
+        Beverage beverage = beverages.get(target);
+
+        if (beverage.price > remainderMoney) {
             return "잔액부족"; // return하게되면 메서드는 거기서 종료. 더 이상 밑으로 안내려감.
         }
-        if (beverageNum.get(target) <= 0) {
+        if (beverage.quantity <= 0) {
             return "수량부족";
         }
 
         // 증감연산의 경우 짧게 표현하는 방법 존재
-        remainderMoney -= beveragePrices.get(target); // remainderMoney = remainderMoney - beveragePrices[target];
+        remainderMoney -= beverage.price; // remainderMoney = remainderMoney - beveragePrices[target];
 //        beverageNum[target] -= 1; // beverageNum[target] = beverageNum[target] - 1;
-        beverageNum.set(target, beverageNum.get(target) - 1);
-        return beverageNames.get(target);
+        beverage.quantity -= 1;
+        return beverage.name;
     }
 
     public int getRemainder() {
         return remainderMoney;
     }
 }
-
