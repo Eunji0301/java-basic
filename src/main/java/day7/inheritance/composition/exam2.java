433e8b310c518e15ef50042864e60c8f076704d2
Eunji0301 <forstudy.eunji@gmail.com>
2024-09-02 13:32:10 +0900

    CompositonExam

---
+++ src/main/java/day7/inheritance/composition/exam2.java
@@ -0,0 +1,23 @@
+package day7.inheritance.composition;
+
+public class exam2 {
+    public static void main(String[] args) {
+        사람 a사람 = new 사람();
+        a사람.나이 = 20;
+        a사람.a왼팔 = new 팔(); // a왼팔은 원시타입이 아닌 객체타입
+
+        System.out.println(a사람.a왼팔.길이 + "cm"); // 객체 안에 객체가 있을 수 있다 => Composition
+        // 출력 : 100cm
+
+        // 객체가 가지고 있는 변수, 메서드
+    }
+}
+
+class 사람 {
+    int 나이;
+    팔 a왼팔;
+}
+
+class 팔 {
+    int 길이 = 100;
+}
