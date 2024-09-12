72346034c251a6f3a65b3ea92280cd79974d3500
Eunji0301 <forstudy.eunji@gmail.com>
2024-09-12 11:20:25 +0900

    FileTest

---
+++ src/main/java/day15/file/FileTest.java
@@ -0,0 +1,21 @@
+package day15.file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileTest {
+    public static void main(String[] args) {
+        String content = "이것은 파일에 쓰여질 내용입니다.";
+        Path filePath = Paths.get("example.txt");
+
+        try {
+            // 파일에 문자열 쓰기
+            Files.write(filePath,content.getBytes());
+            System.out.println("파일 작성 완료 : "+filePath.toAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
